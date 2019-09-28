146a507ed70d14f8c2eda1b908f769f5a490ddce
X1A0 <x1a0@MacBook-Pro-de-German.local>
2019-09-28 16:15:30 -0500

    [Add] react for categories

--- frontend/src/app/sevices/categories.service.ts
+++ frontend/src/app/sevices/categories.service.ts
@@ -8,23 +8,27 @@ export class CategoriesService {
   constructor() { }
 
   get(){
-    return [{
-      category: 'Agro',
-      product: 'Coffe',
-      description: 'An association of coffe producers in Risaralda',
-      img: 'the url'
-      },
+    return [
       {
       category: 'Agro',
       product: 'Coffe',
       description: 'An association of coffe producers in Risaralda',
       img: 'the url'
-      },
-      {
-      category: 'Agro',
-      product: 'Coffe',
-      description: 'An association of coffe producers in Risaralda',
-      img: 'the url'
-      }      
+      }, 
 
+      {
+      category: 'Agro',
+      product: 'Coffe',
+      description: 'An association of coffe producers in Risaralda',
+      img: 'the url'
+      },
+
+      {
+      category: 'Agro',
+      product: 'Coffe',
+      description: 'An association of coffe producers in Risaralda',
+      img: 'the url'
+      },    
+    ]
+  }
 }
