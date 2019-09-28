990f87cff6c5920836e336304ddf2ea1c11ab028
daniel.andres.dev@gmail.com <Daniel Andres>
2019-09-28 14:18:59 -0500

    router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CategoriesComponent },
+  {path: 'products', component: ProductsComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
