bf99457a791fa2670f2fc4dd50796bedf54e8488
daniel.andres.dev@gmail.com <Daniel Andres>
2019-09-28 13:21:58 -0500

    page

--- frotend/src/app/app.module.ts
+++ frotend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CatergoriesComponent } from './catergories/catergories.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CatergoriesComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
