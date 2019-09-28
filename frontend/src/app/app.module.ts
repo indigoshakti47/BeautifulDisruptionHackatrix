5e26e47266fbf01adcef775050ffb00073fb590e
daniel.andres.dev@gmail.com <Daniel Andres>
2019-09-28 13:47:14 -0500

    jada

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { CategoriesComponent } from './categories/categories.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigatorComponent,
+    CategoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
