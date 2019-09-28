f6e03753fe079f360a91a9b49f8980bec41e551b
daniel.andres.dev@gmail.com <Daniel Andres>
2019-09-28 17:26:36 -0500

    ajuste

--- frontend/src/app/sevices/categories.service.ts
+++ frontend/src/app/sevices/categories.service.ts
@@ -10,32 +10,28 @@ export class CategoriesService {
   get(){
     return [
       {
-      category: 'Agro',
-      product: 'Coffe',
-      description: 'An association of coffe producers in Risaralda',
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIRLnnxCiKxHi419IVM0YSxpMafkKZLgWgDMjzhbsEe3qsIzMI8g'
-      },
-
-      {
-      category: 'Textiles',
-      product: 'Coffe',
-      description: 'An association of coffe producers in Risaralda',
-      img: 'http://olsimex.com.mx/images/servicio-transporte/importacion-de-textiles.jpg'
-      },
-
-      {
-      category: 'Metalurgica',
-      product: 'Coffe',
-      description: 'An association of coffe producers in Risaralda',
-      img: {
-        'background': 'url(http://elagonista.com/uploads/noticias/5/2019/08/20190828141003_industria-metalurgica-730x405.jpg)'
-      },
+        category: 'Agro',
+        product: 'Coffe',
+        description: 'An association of coffe producers in Risaralda',
+        img: {
+          'background': 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIRLnnxCiKxHi419IVM0YSxpMafkKZLgWgDMjzhbsEe3qsIzMI8g)'
+        },
       },
       {
-      category: 'Metalurgica',
-      product: 'Coffe',
-      description: 'An association of coffe producers in Risaralda',
-      img: 'http://elagonista.com/uploads/noticias/5/2019/08/20190828141003_industria-metalurgica-730x405.jpg'
+        category: 'Textiles',
+        product: 'Coffe',
+        description: 'An association of coffe producers in Risaralda',
+        img: {
+          'background': 'url(http://olsimex.com.mx/images/servicio-transporte/importacion-de-textiles.jpg)'
+        },
+      },
+      {
+        category: 'Metalurgica',
+        product: 'Coffe',
+        description: 'An association of coffe producers in Risaralda',
+        img: {
+          'background': 'url(http://elagonista.com/uploads/noticias/5/2019/08/20190828141003_industria-metalurgica-730x405.jpg)'
+        },
       },
     ]
   }
